0a4712e1b27b4facb1c5f85e021f9444f6595554
CarlosLukass <carlos.lukassf@gmail.com>
2025-04-11 13:31:17 -0400

    change banner copy

--- cookiesbanner.js
+++ cookiesbanner.js
@@ -18,7 +18,7 @@
     );
     return match ? match[1] : null;
   }
-
+  //
   function enableTrackingScripts() {
     const scripts = document.querySelectorAll(
       'script[type="text/plain"][data-cookieconsent="tracking"]'
@@ -43,7 +43,7 @@
         <div style="width:100%; border-radius: 16px; max-width:520px; background-color:#0e0e0e; background:url(https://images.clickfunnels.com/9f/db7f8363154ca0819be8bd088a6333/Background-Testimonials-1-.jpg); background-size: cover; color:#fff; padding:30px; text-align:left; font-family:sans-serif; z-index:100000; box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;">
           <h3 style="margin-top: 0px">🍪 Cookie Settings</h3>
           <p style="font-size:14px; color:#ccc; margin: 10px 0px; line-height: 1.4">
-            We use Cookies to process personal data, guarantee your safety and improve your experience on this site. By clicking “Accept All”, you accept these Cookies. For detailed information about how we collect and use data, please refer to our <a style="color: #fff; opacity: 0.9;" href="${termsPolicyUrl}" target="_blank" rel="noopener">privacy policy</a>.
+            We use Cookies to process personal data & improve your experience on our site. By clicking “Accept All”, you accept these Cookies. For more details  on how we collect & use data, please see our <a style="color: #fff; opacity: 0.9;" href="${termsPolicyUrl}" target="_blank" rel="noopener">privacy policy</a>.
           </p>
           <div style="margin-top: 20px; text-align:right">
             <button id="declineCookies" style="background:transparent; color:#ccc; border:none; padding:8px 12px; cursor:pointer;">Decline</button>
